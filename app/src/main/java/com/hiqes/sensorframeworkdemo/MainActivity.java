8421193c1abd85d0d91c22f5b20dafb4393a1eab
KPBird <ketanbparmar@yahoo.com>
2014-12-20 10:45:35 +0530

    Update

--- app/src/main/java/com/hiqes/sensorframeworkdemo/MainActivity.java
+++ app/src/main/java/com/hiqes/sensorframeworkdemo/MainActivity.java
@@ -1,43 +1,74 @@
 package com.hiqes.sensorframeworkdemo;
 
-import android.content.Intent;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-
-import java.util.List;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity implements AdapterView.OnItemClickListener {
+public class MainActivity extends ActionBarActivity implements SensorEventListener {
 
     private SensorManager mSensorManager;
-    private SensorAdapter mAdapter;
-    private ListView lstSensors;
-    private List<Sensor> arrSensors;
+    private Sensor mSensor;
+    private TextView txtSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtSensor = (TextView) findViewById(R.id.txtSensor);
+
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        arrSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
-
-        lstSensors = (ListView) findViewById(R.id.lstsensor);
-        mAdapter = new SensorAdapter(this, arrSensors);
-        lstSensors.setAdapter(mAdapter);
-        lstSensors.setOnItemClickListener(this);
-
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        if (mSensor == null) {
+            txtSensor.setText("Light Sensor is not available in the device");
+        }
     }
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Intent intent = new Intent(this,SensorData.class);
-        Sensor s = arrSensors.get(position);
-        intent.putExtra("Type",s.getType());
-        startActivity(intent);
+    protected void onResume() {
+        super.onResume();
+        if (mSensor != null) {
+            mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mSensor != null) {
+            mSensorManager.unregisterListener(this);
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Name: ");
+        sb.append(mSensor.getName());
+        sb.append("\n");
+        sb.append("Vendor: ");
+        sb.append(mSensor.getVendor());
+        sb.append("\n");
+        sb.append("Version: ");
+        sb.append(mSensor.getVersion());
+        sb.append("\n");
+        sb.append("Type: ");
+        sb.append(mSensor.getType());
+        sb.append("\n");
+        sb.append("Timestamp: ");
+        sb.append(event.timestamp);
+        sb.append("\n");
+        sb.append("Value: ");
+        sb.append(event.values[0]);
+        txtSensor.setText(sb.toString());
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
